3edadcc43fe36d7f4a1dade6b7611066f0463c92
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-03 19:17:40 -0300

    Project init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <section class="container">
+      <SectionTitle title="Catálogo de Produtos" />
+    </section>
+  </main>
+</template>
+
+<script>
+import 'boxicons';
+import SectionTitle from './components/SectionTitle.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SectionTitle
+  }
+};
+</script>
---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2 class="sectionTitle">{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/abstracts/variables' as *;
+
+.sectionTitle {
+  position: relative;
+  font-size: $h2-font-size;
+  color: $title-color;
+  margin-bottom: $mg-2-5;
+  text-align: center;
+
+  &:after {
+    content: '';
+    position: absolute;
+    width: 56px;
+    height: 0.18rem;
+    top: -1rem;
+    left: 0;
+    right: 0;
+    margin: auto;
+    background-color: $title-color;
+  }
+}
+</style>
