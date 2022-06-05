5f6b95879165703d3bdf45d512bfb79dd3862252
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-04 23:59:26 -0300

    baseURL defined

---
+++ src/api/axios.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api-vuex-store.herokuapp.com/'
+  // ☝🏽 Se quiser rodar apenas localmente, substitua por: "http://localhost:3000"
+});
