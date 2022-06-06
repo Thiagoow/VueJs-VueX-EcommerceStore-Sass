9616825b0c9c77241ed688075b37ba8d314194f1
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-06 15:00:37 -0300

    Header applied to request 😁

--- src/main.js
+++ src/main.js
@@ -3,18 +3,22 @@ import App from './App.vue';
 
 createApp(App).mount('#app');
 
-/*===== Insert products data into API: 
+/*===== Insert products data into API: */
 import axios from './api/axios';
 import productsArray from './api/generateProducts';
 console.log(productsArray);
 
+const headers = {
+  'Content-Type': 'text/plain; charset=utf-8'
+};
+
 for (let i = 0; i < productsArray.length; i++) {
   axios
-    .post('/products', productsArray[i])
+    .post('/products', productsArray[i], headers)
     .then(function (response) {
       console.log(response);
     })
     .catch(function (error) {
       console.log(error);
     });
-}*/
+}
