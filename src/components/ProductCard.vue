7fd1caff8b93d43bbf38d8edab24b6ebe8d950a3
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-06 22:30:46 -0300

    Styles applied & responsive :D

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,202 @@
+<template>
+  <article class="productCard">
+    <img class="productImg" :src="imgUrl" alt="random image" />
+
+    <div class="productDetails">
+      <span class="name">
+        {{ name }}
+        <b>~ {{ price }}</b>
+      </span>
+
+      <p class="price">{{ price }}</p>
+      <p class="description">
+        {{ description }}
+      </p>
+    </div>
+
+    <div class="moreMinusBtns">
+      <button class="increaseBtn btns">
+        <Icon icon="mdi:plus" />
+      </button>
+
+      <button class="decreaseBtn btns">
+        <Icon icon="mdi:minus" />
+      </button>
+    </div>
+
+    <button class="removeBtn btns">
+      <Icon icon="mdi:selection-ellipse-remove" />
+    </button>
+  </article>
+</template>
+
+<script>
+import { Icon } from '@iconify/vue';
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    imgUrl: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: String || Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    Icon
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/abstracts/variables' as *;
+
+.productCard {
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: row;
+  padding: 1rem;
+  margin-top: $mg-1;
+  margin-bottom: $mg-1-5;
+  background-color: $container-color;
+  box-shadow: 0 4px 14px $shadow-color;
+  border-radius: $border-inside-radius;
+
+  .productImg {
+    width: 25%;
+    height: auto;
+  }
+
+  @media screen and (max-width: 421px) {
+    .moreMinusBtns {
+      margin-left: $mg-1 !important;
+    }
+  }
+  @media screen and (max-width: 437px) {
+    .description {
+      display: none;
+    }
+    .moreMinusBtns {
+      margin-right: 1.5rem !important;
+      margin-left: $mg-3;
+
+      display: flex;
+
+      .decreaseBtn {
+        margin-bottom: $mg-1;
+        margin-left: $mg-0-75;
+      }
+    }
+
+    .name b {
+      display: none;
+    }
+
+    .productDetails .price {
+      display: flex;
+    }
+  }
+  @media screen and (max-width: 469px) {
+    .moreMinusBtns {
+      margin-right: $mg-2;
+
+      .increaseBtn {
+        margin-bottom: $mg-1-75;
+        margin-top: $mg-0-75;
+      }
+    }
+
+    .removeBtn {
+      margin-bottom: 0.3rem;
+    }
+  }
+  @media screen and (min-width: 506px) {
+    .productDetails .description {
+      width: 90%;
+    }
+  }
+  @media screen and (max-width: 642px) {
+    .productImg {
+      display: none;
+    }
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: $mg-2-5;
+    margin-bottom: $mg-0-5;
+    max-width: 664px;
+    max-height: 134px;
+    margin-left: auto;
+    margin-right: auto;
+    .removeBtn {
+      margin-bottom: 0.6rem;
+    }
+  }
+}
+
+.productDetails {
+  align-self: center;
+  margin-left: $mg-1;
+
+  .name {
+    font-weight: $wgt-semi-bold;
+  }
+  .name b,
+  .price {
+    color: #0062be;
+    font-size: $txt-font-size;
+    font-weight: $wgt-semi-bold;
+  }
+
+  .price {
+    display: none;
+  }
+
+  .description {
+    margin-top: $mg-0-5;
+    width: 100%;
+  }
+}
+
+.btns {
+  background: transparent;
+  font-size: $mg-1-75;
+  display: flex;
+  align-self: center;
+}
+
+.moreMinusBtns {
+  margin-right: $mg-2-5;
+
+  .increaseBtn {
+    margin-bottom: $mg-1-5;
+    margin-top: $mg-0-5;
+  }
+
+  .increaseBtn:hover,
+  .decreaseBtn:hover {
+    opacity: 0.4;
+  }
+}
+
+.removeBtn {
+  color: $second-color;
+  font-size: $mg-2;
+  //👇🏽 top right left bottom
+  padding: 0 1rem 0rem 0;
+
+  &:hover {
+    color: $alt-second-color;
+  }
+}
+</style>
--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,11 +1,33 @@
 <template>
   <main>
-    <h1>Cart</h1>
+    <section class="productsContainer">
+      <ProductCard
+        name="Fulano Ciclano"
+        imgUrl="https://picsum.photos/seed/nomeProduto/536/354"
+        price="R$ 15,00"
+        description="Fulano Ciclano random description. Lorem ipsum dolor."
+      />
+    </section>
   </main>
 </template>
 
-<script></script>
+<script>
+import ProductCard from '../components/ProductCard.vue';
+
+export default {
+  components: {
+    ProductCard
+  }
+};
+</script>
 
 <style lang="scss" scoped>
 @use '../assets/scss/abstracts/variables' as *;
+
+.productsContainer {
+  padding: 2rem;
+  display: flex;
+  background-color: $body-color;
+  flex-direction: column;
+}
 </style>
