c1cbf8202074f1bdc1e80580e6e81637582bc07c
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-07 01:14:01 -0300

    VueX store created in the right way

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-Vue.use(Vuex);
+import { createStore } from 'vuex';
 
 import state from './state';
 import * as getters from './getters';
 import * as mutations from './mutations';
 import * as actions from './actions';
 
-export default new Vuex.Store({
+export default createStore({
   state,
   getters,
   mutations,
