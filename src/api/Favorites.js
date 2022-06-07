2018477a676a8e9f239edc153a7996a972655df4
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-07 01:40:28 -0300

    Headers defined on Requests :D

--- src/api/Favorites.js
+++ src/api/Favorites.js
@@ -1,20 +1,27 @@
 import axios from './axios';
 const END_POINT = 'favorites';
 
+const headers = {
+  'Content-Type': 'text/plain; charset=utf-8',
+  //CORS with my ProxyServer that enables it:
+  'Access-Control-Allow-Origin':
+    'https://cors-proxyserver-thiagoow.herokuapp.com/'
+};
+
 export default {
   all() {
-    return axios.get(END_POINT);
+    return axios.get(END_POINT, headers);
   },
 
   store(data) {
-    return axios.post(END_POINT, data);
+    return axios.post(END_POINT, data, headers);
   },
 
   delete(id) {
-    return axios.delete(`${END_POINT}/${id}`);
+    return axios.delete(`${END_POINT}/${id}`, headers);
   },
 
   deleteAll() {
-    return axios.delete(END_POINT);
+    return axios.delete(END_POINT, headers);
   }
 };
