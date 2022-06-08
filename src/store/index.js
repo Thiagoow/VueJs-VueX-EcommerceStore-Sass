4aa49cfdb3fdb78c82a1735b3e61a7a65a78dc36
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-08 18:25:28 -0300

    Comment added

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,16 @@ import { createStore } from 'vuex';
 import state from './state';
 import * as getters from './getters';
 import * as mutations from './mutations';
-import * as actions from './localActions';
+import * as actions from './remoteActions';
+
+/* IF using localActions: 
+add params to function inside all mounted() 
+
+  IF using remoteActions:
+remove params in function inside all mounted()
+-------------------&&------------------
+change function insertOnVueX to insertOnAPI 
+on AppStateBtns component */
 
 export default createStore({
   state,
