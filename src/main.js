53d2546c29b773db2bc038dc62a22460907d535b
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-08 17:33:32 -0300

    File cleared

--- src/main.js
+++ src/main.js
@@ -7,27 +7,3 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 app.mount('#app');
-
-/*===== Insert products data into API: 
-import axios from './api/axios';
-import productsArray from './api/generateProducts';
-console.log(productsArray);
-
-const headers = {
-  'Content-Type': 'text/plain; charset=utf-8',
-  //CORS with my ProxyServer that enables it:
-  'Access-Control-Allow-Origin':
-    'https://cors-proxyserver-thiagoow.herokuapp.com/'
-};
-
-for (let i = 0; i < productsArray.length; i++) {
-  axios
-    .post('/products', productsArray[i], headers)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-*/
