51e1c3f93dfb799949626b7697f9730c7fb32df7
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-22 20:21:53 -0300

    Fetching poster & backdrop images

--- src/api/generateProducts.js
+++ src/api/generateProducts.js
@@ -17,7 +17,8 @@ for (let i = 0; i < productsArray.length; i++) {
   productsArray[i] = {
     id: i + 1,
     name: `${results[i].name}`,
-    imgUrl: `https://image.tmdb.org/t/p/w200${results[i].backdrop_path}`,
+    posterUrl: `https://image.tmdb.org/t/p/w200${results[i].poster_path}`,
+    backdropUrl: `https://image.tmdb.org/t/p/w200${results[i].backdrop_path}`,
     description: `${results[i].overview.substring(0, 80)}...`,
     price: 10 + results[i].vote_average
   };
