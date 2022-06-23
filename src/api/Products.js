fcbd35d16613d84975a08e103ebd82a541fca51b
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-06-23 14:46:04 -0300

    deleteAll requests repaired on axios routes

--- src/api/Products.js
+++ src/api/Products.js
@@ -17,7 +17,9 @@ export default {
     return axios.get(`${END_POINT}/${id}`, headers);
   },
 
-  deleteAll() {
-    return axios.delete(END_POINT, headers);
+  deleteAll(IDsArray) {
+    IDsArray.forEach((id) => {
+      axios.delete(`${END_POINT}/${id}`, headers);
+    });
   }
 };
