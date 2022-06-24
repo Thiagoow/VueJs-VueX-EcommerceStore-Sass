f1d633dba40026453575c51af323f9ea75796383
Thiago Silva Lopes <71602458+Thiagoow@users.noreply.github.com>
2022-06-24 16:52:44 -0300

    Instructions to use remoteActions enhanced

--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,12 @@ import * as actions from './localActions';
 
 /* IF using localActions: 
 add params to function inside all mounted() 
-
   IF using remoteActions:
 remove params in function inside all mounted()
 -------------------&&------------------
 change function insertOnVueX to insertOnAPI 
++ change clear functions on Cart & Favorites Page
++ insert IDsArray inside () on clearAllRoutes()
 on AppStateBtns component */
 
 export default createStore({
